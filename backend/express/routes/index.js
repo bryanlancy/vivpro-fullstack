eab43f6660c661760c143065b29688404e298928
bryanlancy <bryanburns93@gmail.com>
2021-08-03 19:25:09 -0400

    search endpoint

--- backend/express/routes/index.js
+++ backend/express/routes/index.js
@@ -3,26 +3,36 @@ const router = require('express').Router()
 const playlist = require('../files/playlist.json')
 
 const normal = {}
+const searchDict = {}
 for (const prop in playlist) {
     for (const song in playlist[prop]) {
         const value = playlist[prop][song]
         normal[song] ? normal[song][prop] = value : normal[song] = { [prop]: value }
+        if (prop == 'title') {
+            searchDict[value] = song
+        }
     }
+
 }
 
 router.get('/playlist', (req, res) => {
     //page is 0-index
     const { songs_per_page, page } = req.query
-    const response = {}
+    const playlist = {}
     if (songs_per_page) {
         const start = page * songs_per_page || 0
         for (let i = start; i < start + parseInt(songs_per_page); i++) {
-            response[i] = { ...normal[i] }
+            playlist[i] = { ...normal[i] }
         }
     }
-    else response = normal
-    res.json(response)
+    else playlist = normal
+    res.status(200).json({ playlist })
 })
 
+router.get('/search', (req, res) => {
+    const { title } = req.query
+    const result = normal[searchDict[title]]
+    res.json({ result, searchDict })
+})
 
 module.exports = { apiRouter: router }
